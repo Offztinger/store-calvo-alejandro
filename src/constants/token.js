e7b3e2ddb479d5aad19105db51c581483af181a5
Luis Alejandro Calvo Álvarez <Offztinger@gmail.com>
2021-09-23 21:44:17 -0500

    agregando deploy

--- src/constants/token.js
+++
@@ -1,3 +0,0 @@
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiZTc3YWM1NGQ5NjAwMWEzYmIxYzIiLCJpYXQiOjE2MzIzNjQ0MTB9.aVwi4YruTq5zmJ_GVrYqojHBEsrLS1I9FP5X_sVFewE";
-export default token;
